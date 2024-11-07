b62c5ededb03af1fb6f6b9f972dfcdf9a33aeaa7
Mutual-Luo <147460514+Mutual-Luo@users.noreply.github.com>
2024-11-07 14:53:24 +0800

    Update 1_📅_Timeline.py

--- pages/1_📅_Timeline.py
+++ pages/1_📅_Timeline.py
@@ -10,7 +10,7 @@ st.markdown("### 📅Timeline")
 st.markdown(
 """
 * Step 1 - Data Release, Platform setup and solicitation of challenge participants, May 1, 2024
-* Step 2 - Deadline for contest teams to submit final solutions, November 10, 2024
-* Step 3 - Announcement of winning teams, Nov 25, 2024
+* Step 2 - Deadline for contest teams to submit final solutions, November 21, 2024
+* Step 3 - Announcement of winning teams, Nov 30, 2024
 """
 )
